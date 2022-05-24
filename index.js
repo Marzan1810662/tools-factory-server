02eae16229c75213ee4c782827c15db44dfb1606
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-25 01:53:32 +0600

    cors problem

--- index.js
+++ index.js
@@ -6,10 +6,25 @@ require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
 
-app.use(cors());
+const port = process.env.PORT || 5000;
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+
+// app.use(cors());
 app.use(express.json());
+app.use(cors(corsConfig))
+app.options("*", cors(corsConfig))
+app.use(express.json())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+    next()
+})
+
 
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
